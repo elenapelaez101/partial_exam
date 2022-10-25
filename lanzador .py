b624d44276ccf8e8636cb68af8d16d89e05ef74a
elenapelaez101 <elena.pelaezsanchez@usp.ceu.es>
2022-10-25 16:39:31 +0200

    realizando menu

--- lanzador .py
+++ lanzador .py
@@ -1,3 +1,15 @@
-from importlib.resources import path
+
+
+
 import.sys
 sys.path.insert(0,path)
+
+def menu():
+    print("MENÚ")
+    print("1.Ejercicio 1")
+    print("2.Ejercicio 2")
+    print("3.Ejercicio 3")
+    print("4.Ejercicio 4")
+    print("5.Ejercicio 5")
+    eleccion = int(input("Seleccione el ejercicio que quiera ver:"))
+    
